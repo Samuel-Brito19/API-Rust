33d66c7779e0e543734b2657ca54c598d58a1120
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-18 08:33:10 -0300

    Creating the controllers

---
+++ src/controllers.rs
@@ -0,0 +1,36 @@
+use std::sync::Arc;
+
+use actix_web::{web, HttpResponse};
+
+use crate::db::{AppQueue, CreatePerson};
+
+use deadpool_postgres::Pool;
+
+pub type APIResult = Result<HttpResponse, Box<dyn std::error::Error>>;
+
+#[actix_web::post("/people")]
+
+pub async fn create_person(
+    redis_pool: web::Data<deadpool_redis::Pool>,
+    payload: web::Json<CreatePerson>,
+    queue: web::Json<Arc<AppQueue>>,
+) -> APIResult {
+    match validate_payload(&payload) {
+        Some(response) => return Ok(response),
+        None => (),
+    };
+
+    let redis_key = format!("a/{}", payload.nickname.clone());
+    match get_redis(&redis_pool, &redis_key).await {
+        Ok(_) => return Ok(HttpResponse::UnprocessableEntity().finish()),
+        Err(_) => (),
+    };
+    let id = uuid::Uuid::new_v4().to_string();
+    let dto = create_dto_and_queue(payload, &id, queue.clone());
+    let body = serde_json::to_string(&dto)?;
+    let _ = set_redis(&redis_pool, &id, &body).await;
+
+    Ok(HttpResponse::Created()
+        .append_header(("Location", format!("/people/{id}")))
+        .finish())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod controllers;
 mod db;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
